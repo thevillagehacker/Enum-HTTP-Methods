6b28a0bf2d6c47b579a3c14e97adc3acbe930174
Naveen J <nvnj1998@gmail.com>
2022-12-18 10:54:27 +0530

    Update methods.py

--- src/python/methods.py
+++ src/python/methods.py
@@ -2,11 +2,30 @@ import argparse
 import requests
 from termcolor import colored
 
+#banner
+def print_banner(banner_text):
+  banner_length = len(banner_text)
+  print(" " * banner_length)
+  print(banner_text)
+  print(" " * banner_length)
+print_banner("""
+          _|_|_  _   .|| _  _  _ |_  _  _|  _  _
+           | | |(/_\/|||(_|(_|(/_| |(_|(_|<(/_|
+                            _|
+                  ------------------
+               ~ |Do Hacks to Secure| ~
+                  ------------------
+            https://twitter.com/thevillagehackr
+""")
+
 # Parse command line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--host", required=True, help="The host to test")
 parser.add_argument("--headers", help="Authorization headers to include in the requests (format: 'key1:value1,key2:value2')")
 args = parser.parse_args()
+
+print("[+] Host:", colored(args.host, "yellow"))
+print("[+] Headers:", args.headers)
 
 # Build the headers dictionary
 headers = {}
